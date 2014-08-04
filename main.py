d84a5bbbecd7adc2abac158a1754ef9040a8eb5a
John Raffensperger <massterchief24@gmail.com>
2014-08-04 07:28:59 -0400

    Added a Kalman Filter and a Motion Model class

--- main.py
+++ main.py
@@ -1,16 +1,118 @@
 from math import *
+from random import *
+import numpy
+
+class KalmanFilter(object):
+
+	def __init__(self, worldDimensions, uncertainty=0.1):
+		super(KalmanFilter, self).__init__()
+		width = worldDimensions[0]
+		height = worldDimensions[1]
+
+		self.P =  numpy.matrix([[1000., 0., 0., 0.], [0., 1000., 0., 0.], [0., 0., 1000., 0.], [0., 0., 0., 1000.]]) # initial uncertainty
+		self.F =  numpy.matrix([[1., 0., 1.0, 0.], [0., 1., 0., 1.0], [0., 0., 1., 0.], [0., 0., 0., 1.]]) # next state function
+		self.H =  numpy.matrix([[1., 0., 0., 0.], [0., 1., 0., 0.]]) # measurement function
+		self.R =  numpy.matrix([[uncertainty, 0.], [0., uncertainty]]) # measurement uncertainty
+		self.I =  numpy.matrix([[1., 0., 0., 0.], [0., 1., 0., 0.], [0., 0., 1., 0.], [0., 0., 0., 1.]]) # identity numpy.matrix
+
+		self.x = numpy.matrix([[randint(0, width)], [randint(0, height)], [0.], [0.]]) # initial state (location and velocity)
+		self.u = numpy.matrix([[random() * 4 - 2], [random() * 4 - 2], [0.], [0.]]) # external motion
+
+		self.u = numpy.matrix([[0.], [0.], [0.], [0.]])
+		self.x = numpy.matrix([[0.], [0.], [0.], [0.]])
+
+	def updateFilter(self, measurement):
+
+		# prediction
+		self.x = (self.F * self.x) + self.u
+ 		self.P = self.F * self.P * numpy.transpose(self.F)
+        
+        # measurement update
+		self.Z = numpy.matrix([measurement])
+		self.y = numpy.transpose(self.Z) - (self.H * self.x)
+		self.S = self.H * self.P * numpy.transpose(self.H) + self.R
+		self.K = self.P * numpy.transpose(self.H) * numpy.linalg.inv(self.S)
+		self.x = self.x + (self.K * self.y)
+		self.P = (self.I - (self.K * self.H)) * self.P
+        
+
+	def measurement_prob(self, measurement):
+		new_x = self.x.tolist()[0][0] + self.x.tolist()[2][0]
+		new_y = self.x.tolist()[1][0] + self.x.tolist()[3][0]
+
+		return self.distanceBetween([new_x, new_y], measurement)
+
+	def getPrediction(self):
+		return self.x
+
+	def distanceBetween(self, point1, point2):
+		"""Computes distance between point1 and point2. Points are (x, y) pairs."""
+		x1, y1 = point1
+		x2, y2 = point2
+		return sqrt((x2 - x1) ** 2 + (y2 - y1) ** 2)
 
 class ForwardMotionModel(object):
+	
+	previousParticles = []
+	N = 0
+	worldDimensions = ()
+
 	"""Represents the motion model for when the hexbug is moving forward"""
-	def __init__(self):
+	def __init__(self, worldDimensions, numberOfParticles=100):
 		super(ForwardMotionModel, self).__init__()
+		self.N = numberOfParticles
+		self.worldDimensions = worldDimensions
 	
 	def update(self, position):
-		pass
+		# --------
+		#
+		# Make particles
+		# 
+		if self.previousParticles == []:
+			for i in range(self.N):
+				r = KalmanFilter(self.worldDimensions)
+				self.previousParticles.append(r)
+
+		newParticles = []
+
+		# --------
+		#
+		# Update particles
+		#     
+
+		# measurement update
+		w = []
+		for i in range(len(self.previousParticles)):
+			w.append(self.previousParticles[i].measurement_prob(position))
+			self.previousParticles[i].updateFilter(position)
+
+		# resampling
+		p3 = []
+		index = int(random() * self.N)
+		beta = 0.0
+		mw = max(w)
+		for i in range(self.N):
+			beta += random() * 2.0 * mw
+			while beta > w[index]:
+				beta -= w[index]
+				index = (index + 1) % self.N
+			p3.append(self.previousParticles[index])
+
+		self.previousParticles = p3
+	    
 
 	def getNextLocation(self):
-		pass
-
+		sum_x = 0
+		sum_y = 0
+		for i in range(len(self.previousParticles)):
+			p = self.previousParticles[i]
+			next_x = p.getPrediction().tolist()[0][0] + p.getPrediction().tolist()[2][0]
+			next_y = p.getPrediction().tolist()[1][0] + p.getPrediction().tolist()[3][0]
+			sum_x += next_x
+			sum_y += next_y
+			print p.getPrediction()
+		return (sum_x / len(self.previousParticles), sum_y / len(self.previousParticles))
+		
 	def getForwardSpeed(self):
 		pass
 
@@ -31,17 +133,19 @@ class Tracker(object):
 	rightWall  = 0  # pixel representing the x value of the right wall
 	topWall    = 0  # pixel representing the y value of the top wall
 	bottomWall = 0  # pixel representing the y value of the bottom wall
+	worldDimensions = []
 
 	def __init__(self, worldDimensions):
 		self.leftWall = worldDimensions[0]/2
 		self.rightWall = worldDimensions[0]/2
 		self.topWall = worldDimensions[1]/2
 		self.bottomWall = worldDimensions[1]/2
+		self.worldDimensions = worldDimensions
 
 	"""The main tracking function... Takes the data points and figures out the 
 	    world and the motion of the hexbug. """
 	def trackRobot(self, data):
-		forward = ForwardMotionModel()
+		forward = ForwardMotionModel(self.worldDimensions)
 		collision = CollisionMotionModel()
 
 		previousLocation = [0, 0]
@@ -56,11 +160,14 @@ class Tracker(object):
 			
 			if self.didCollide(previousLocation, location) :
 				collision.update(location)
+				formard = ForwardMotionModel(self.worldDimensions)
 			
 			else : 
 				forward.update(location)
 
 			previousLocation = location
+
+		return forward.getNextLocation()
 
 	def didCollide(self, previousLocation, location):
 		
@@ -108,37 +215,6 @@ class Tracker(object):
 
 """Main Program"""
 tracker = Tracker([1280, 1024])
-tracker.trackRobot([[665, 156],
-					[663, 147],
-					[660, 139],
-					[657, 129],
-					[653, 121],
-					[649, 112],
-					[644, 104],
-					[639, 96],
-					[636, 93],
-					[632, 91],
-					[627, 91],
-					[619, 92],
-					[610, 92],
-					[601, 92],
-					[591, 91],
-					[580, 91],
-					[571, 92],
-					[561, 92],
-					[552, 92],
-					[543, 92],
-					[533, 93],
-					[523, 91],
-					[513, 90],
-					[504, 89],
-					[494, 87],
-					[486, 89],
-					[478, 90],
-					[469, 91],
-					[461, 93],
-					[453, 96],
-					[444, 99],
-					[435, 103],])
-
-print tracker.getWallCoordinates()
+guess = tracker.trackRobot([[x * 10,x * 10] for x in range(10)])
+print "Guess next location: ", guess
+print "Actual next location: ", [292, 418]
